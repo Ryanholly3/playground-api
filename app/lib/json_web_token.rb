b107a80262528bca7b9486bbf90a27f4babfa8cf
Ryan Holly <ryanholly3@gmail.com>
2020-12-08 14:16:52 -0700

    Add in tokens

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -6,16 +6,16 @@ class JsonWebToken
     JWT.decode(token, nil,
                true, # Verify the signature of this token
                algorithm: 'RS256',
-               iss: Rails.application.credentials.auth0[:domain],
+               iss: 'https://lit-spire-32222.herokuapp.com/', # domain
                verify_iss: true,
-               aud: Rails.application.credentials.auth0[:api_identifier],
+               aud: 'https://dev-zcws25jd.us.auth0.com/oauth/token/', # api_identifier
                verify_aud: true) do |header|
       jwks_hash[header['kid']]
     end
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("#{Rails.application.credentials.auth0[:domain]}.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("#{'https://lit-spire-32222.herokuapp.com/'}.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys
